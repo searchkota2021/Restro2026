d0884bd59538c2995b99096642d59d88c1206f6c
searchkota2021 <119028078+searchkota2021@users.noreply.github.com>
2026-05-02 11:26:53 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,2 @@
-
+const mysql = require('mysql2/promise'); require('dotenv').config();
+module.exports = mysql.createPool({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME });
